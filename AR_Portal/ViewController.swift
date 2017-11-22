a3444a7bdf8f18adcf8aec655515b9c84b07d921
Pohle, Sven <spohle@ctx-ar.activision.com>
2017-11-22 11:52:08 -0800

    Initial Commit

---
+++ AR_Portal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AR_Portal
+//
+//  Created by Pohle, Sven on 11/22/17.
+//  Copyright © 2017 Pohle, Sven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
